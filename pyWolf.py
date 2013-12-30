2845c7e69398df367d1ca2edae723e91ae20afac
Jenic Rycr <jenic@wubwub.me>
2013-12-29 20:24:05 -0700

    Input/Output code moved to dedicated module
    First step in attempting to distance game code from being irc specific.
    This should also make testing much easier.

--- pyWolf.py
+++ pyWolf.py
@@ -1,83 +1,61 @@
 # Import modules
-import socket
 import random
+import minipyIRC as IO
 
 # Functions to check for different user inputs
 # ---------------------------------------------------------------------------------- #
 
-# Respond to pings
-def checkPing(msg):
-	if msg.find('PING :') != -1:
-		ircSock.send('PONG :pingis\n')
-
-# Sends messages..
-def sendMsg(target, msg):
-	ircSock.send('PRIVMSG '+ target +' :'+ msg +'\n')
-
-# Checks for new messages
-def checkMessages():
-	ircMsg = ircSock.recv(2048)
-	ircMsg = ircMsg.strip('\n\r')
-	print(ircMsg)
-	return ircMsg
-
-def getNick(msg):
-	exclaimIndex = msg.find('!')
-	nickName = msg[1:exclaimIndex]
-	return nickName
-
 # Check if someone wants to join
 def checkJoin(msg):
 	if msg.find(commands[2]) != -1:
-		joiningUser = getNick(msg)
+		joiningUser = ioh.getNick(msg)
 		if not joiningUser in joinedUsers:
 			joinedUsers.append(joiningUser)
-			sendMsg(channel, '%s has joined!' % joiningUser)
+			ioh.sendMsg(channel, '%s has joined!' % joiningUser)
 
 # Check if someone wants to leave		
 def checkLeave(msg):
 	if msg.find(commands[3]) != -1:
-		leavingUser = getNick(msg)
+		leavingUser = ioh.getNick(msg)
 		if leavingUser in joinedUsers:
 			joinedUsers.remove(leavingUser)
-			sendMsg(channel, '%s has left!' % leavingUser)
+			ioh.sendMsg(channel, '%s has left!' % leavingUser)
 
 # Check to see if a joined user has changed their nickname			
 def checkNick(msg):
 	if msg.find('NICK') != -1:
-		lastNick = getNick(msg)
+		lastNick = ioh.getNick(msg)
 		nickPosition = msg.find('NICK :') + 6
 		newNick = msg[nickPosition:len(msg)]
 		if lastNick in joinedUsers:
-			sendMsg(channel, 'Joined user, %s, has changed their nick. New nick, %s, added to paticipants.' % (lastNick, newNick))
+			ioh.sendMsg(channel, 'Joined user, %s, has changed their nick. New nick, %s, added to paticipants.' % (lastNick, newNick))
 			joinedUsers.remove(lastNick).append(newNick)
 
 # Check to see if someone has requested a list of players			
 def checkList(msg):
 	if msg.find(commands[1]) != -1:
 		playerList = ', '.join(joinedUsers)
-		sendMsg(channel, 'Players: ' + playerList)
+		ioh.sendMsg(channel, 'Players: ' + playerList)
 
 # Check if someone has requested help		
 def checkHelp(msg):
 	if msg.find(commands[4]) != -1:
 		sortedCommands = sorted(commands)
 		commandList = ' '.join(sortedCommands)
-		helpUser = getNick(msg)
-		sendMsg(helpUser, commandList)
+		helpUser = ioh.getNick(msg)
+		ioh.sendMsg(helpUser, commandList)
 # -------------------------------------------------------------------------------------------------- #
 
 # Sets up the game waiting for someone to start it
 def initialize():
 	while 1:
-		message = checkMessages()
-		checkPing(message)
+		message = ioh.checkMessages()
 		
 		# Check to see if someone is attempting to start a new game
 		# If more than three people have joined, start the game. Otherwise, send a message to the channel.
 		if message.find(commands[0]) != -1:
 			if len(joinedUsers) <= 3:
-				sendMsg(channel, 'Not enough players (%i) to start a new game' % len(joinedUsers))
+				ioh.sendMsg(channel, 'Not enough players (%i) to start a new game' % len(joinedUsers))
 			else:
 				return len(joinedUsers)
 				
@@ -89,16 +67,15 @@ def initialize():
 		
 def nightRound(werewolves, villagers):
 	for wolf in werewolves:
-		sendMsg(wolf, 'Choose your victim...')
+		ioh.sendMsg(wolf, 'Choose your victim...')
 
 	wolfVotes = {}
 		
 	while 1:
-		message = checkMessages()
-		checkPing(message)
+		message = ioh.checkMessages()
 		
 		for wolf in werewolves:
-			if getNick(message) == wolf and message.find('PRIVMSG TWP_Wolf :')!= -1:
+			if ioh.getNick(message) == wolf and message.find('PRIVMSG TWP_Wolf :')!= -1:
 				if not wolf in wolfVotes:
 					killIndex = message.index('PRIVMSG TWP_Wolf :') + 18
 					killedUser = message[killIndex:len(message)]
@@ -108,9 +85,9 @@ def nightRound(werewolves, villagers):
 						else:
 							wolfVotes[wolf] = killedUser
 					else:
-						sendMsg(wolf, 'User is already dead!')
+						ioh.sendMsg(wolf, 'User is already dead!')
 				else:
-					sendMsg(wolf, 'You have already picked!')
+					ioh.sendMsg(wolf, 'You have already picked!')
 		
 		if len(wolfVotes) == 2:
 			if wolfVotes[werewolves[0]] == wolfVotes[werewolves[0]]:
@@ -118,17 +95,16 @@ def nightRound(werewolves, villagers):
 		else:
 			wolfVotes = {}
 			for wolf in werewolves:
-				sendMsg(wolf, 'Your votes were different! Try Again.')
+				ioh.sendMsg(wolf, 'Your votes were different! Try Again.')
 		
 		checkNick(message)
 		checkHelp(message)
 		
 def dayRound(killedUser):
-	sendMsg(channel, 'The sun has risen and the wolves are away, but one unlucky person is no loger with us.')
-	sendMsg(channel, 'RIP %s' % killedUser)
+	ioh.sendMsg(channel, 'The sun has risen and the wolves are away, but one unlucky person is no loger with us.')
+	ioh.sendMsg(channel, 'RIP %s' % killedUser)
 	while 1:
-		message = checkMessages()
-		checkPing(message)
+		message = ioh.checkMessages()
 
 def newGame(playercount):
 	# Create empty wolf list
@@ -136,13 +112,13 @@ def newGame(playercount):
 	villagers = joinedUsers
 	
 	def getWolves(wolfCount):
-		sendMsg(channel, '    WEREWOLVES: 1    VILLAGERS: %i' % (int(playercount) - 1))
+		ioh.sendMsg(channel, '    WEREWOLVES: 1    VILLAGERS: %i' % (int(playercount) - 1))
 		for player in random.sample(joinedUsers, wolfCount):
 			villagers.remove(player)
 			werewolves.append(player)
 	
 	# Send starting message and get werewolves
-	sendMsg(channel, '--- STARTING NEW GAME OF WEREWOLF! ---')
+	ioh.sendMsg(channel, '--- STARTING NEW GAME OF WEREWOLF! ---')
 	if playercount <= 6:
 		getWolves(1)
 	elif playercount > 6:
@@ -152,11 +128,11 @@ def newGame(playercount):
 		
 	wolfList = ' '.join(werewolves)
 		
-	sendMsg(channel, 'It grows dark outside. The werewolves are on the hunt!')
+	ioh.sendMsg(channel, 'It grows dark outside. The werewolves are on the hunt!')
 	
 	for wolf in werewolves:
-		sendMsg(wolf, 'You are a werewolf! Use /msg to whisper with the other werewolves (if any) to choose who to kill. After you decide, whisper the nickname of the player you want to kill back to me.')
-		sendMsg(wolf, 'List of wolves: ' + wolfList)
+		ioh.sendMsg(wolf, 'You are a werewolf! Use /msg to whisper with the other werewolves (if any) to choose who to kill. After you decide, whisper the nickname of the player you want to kill back to me.')
+		ioh.sendMsg(wolf, 'List of wolves: ' + wolfList)
 	
 	return werewolves
 
@@ -166,12 +142,8 @@ channel = '#TimeWastePool'
 botNick = 'TWP_Wolf'
 
 # Connect to channel 
-ircSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ircSock.connect((SERVER, 6667))
-ircSock.send('USER '+ botNick +' '+ botNick +' '+ botNick +' :This bot moderates games of Werewolf\n')
-ircSock.send('NICK '+ botNick +'\n')
-ircSock.send('JOIN '+ channel +'\n')
-sendMsg(channel, 'Hi, I\'m ' + botNick + '. I moderate games of Werewolf! If you want to join the game, say "!join". For a list of commands, say "!helpme"')
+ioh = IO.pyIRC(serv=SERVER, autojoin=1, chan=channel, nick=botNick, msg='This bot moderates games of Werewolf')
+ioh.sendMsg(channel, 'Hi, I\'m ' + botNick + '. I moderate games of Werewolf! If you want to join the game, say "!join". For a list of commands, say "!helpme"')
 
 # List of commands available to channel
 commands = ['!startwolf','!list','!join','!leave','!helpme','!quit']
